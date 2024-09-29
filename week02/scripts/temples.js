d7afb9924fd9d3a2f96449c663c3d6db7a8af78a
wcvaughan <chrisvaughanlds@gmail.com>
2024-09-28 18:46:01 -0800

    work work work

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -5,3 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const lastModifiedDate = document.lastModified;
     document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
 });
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
