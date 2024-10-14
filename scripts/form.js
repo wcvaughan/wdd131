e5773df4741e4e212e80da70ce90da71c5effe7b
wcvaughan <chrisvaughanlds@gmail.com>
2024-10-13 22:29:10 -0800

    form work

---
+++ scripts/form.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentyear').textContent = currentYear;
+
+    const lastModifiedDate = document.lastModified;
+    document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
+});
+
