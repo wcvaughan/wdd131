816acaaa5b519e827edd4f85b9b758458741d625
wcvaughan <chrisvaughanlds@gmail.com>
2024-10-13 23:08:45 -0800

    javascript

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const currentYear = new Date().getFullYear();
-    document.getElementById('currentyear').textContent = currentYear;
+  const currentYear = new Date().getFullYear();
+  document.getElementById('currentyear').textContent = currentYear;
 
-    const lastModifiedDate = document.lastModified;
-    document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
+  const lastModifiedDate = document.lastModified;
+  document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
+
+  const productSelect = document.getElementById('product');
+  console.log('Product select element: ', productSelect);
+
+  const products = [
+    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+  ];
+
+  products.array.forEach(product => {
+    console.log('Adding product:', product);
+    const option = document.createElement('option');
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+  });
 });
+
 
